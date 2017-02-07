c4e50d84d01db6afc99b8b59863e78f831ffc702
Kathleen P Chase <kpchase@alionscience.com>
2017-02-07 08:48:46 -0500

    placeholder for new script: use_examples.sh

---
+++ use_examples.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+#
+# ./use_examples.sh
+#
+# For each example.<name> config file:
+#   save a backup of the existing config file
+#   copy the example file to the .<name> config file
+#
+# By: Kathleen P Chase
+#
+######################################################
+
+
